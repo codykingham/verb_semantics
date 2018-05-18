4ee35e12ae4c1fc0c36e8be1eea7f38d4d035c89
Cody Kingham <codyakingham@gmail.com>
2018-05-19 01:22:27 +0200

    continue next gen experiment classes

--- project_code/experiments2.py
+++ project_code/experiments2.py
@@ -9,7 +9,7 @@ convert the coordinated data into token strings for counting.
 This method has many advantages over the first-gen, class-only
 format. With the old format, I gained extensibility
 at the expense of readability and clarity. In the new method,
-we maintain extensibility while maximizing clarity:
+we maintain extensibility while maximizing clarity. This includes:
 
 √ selection parameters for bases and targets clearly described: 
     templates are very easy to read/edit compared with code.
@@ -21,9 +21,13 @@ we maintain extensibility while maximizing clarity:
     places
 '''
 
+import collections
+import numpy as np
+import pandas as pd
+
 class Experiment:
     
-    def __init__(self, tf=None, parameters):
+    def __init__(self, parameters, tf=None, min_observation=10):
         '''
         Parameters is a tuple of tuples.
         Tuples consist of:
@@ -39,43 +43,109 @@ class Experiment:
         basis_tokenizer - a function to construct basis tokens, requires basis_i(s)
         '''
         
+        self.min_obs = min_observation # minimum observation requirement
+         
         # Text-Fabric method short forms
         F, E, T, L, S = tf.F, tf.E, tf.T, tf.L, tf.S
         
-        # experiment_data[target_token][clause][list_of_bases_tokens]
+        # raw experiment_data[target_token][clause][list_of_bases_tokens]
         experiment_data = collections.defaultdict(lambda: collections.defaultdict(list))
         
-        for templ, templ_kw, target, bases, target_token, basis_token in parameters:
-            
-            search_template = templ.format(*templ_kw)
-            sample = S.search(search_template)
-            
-            
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-# Semantic Verb Frame Templates
-good_codes = set(f'1\.00100{i}.*' for i in range(1, 7)) | {'1\.002004\.*'}
-bases=('Subj', 'PrAd', 'Adju', 'Cmpl', 'Loca', 'Time', 'Objc',)
-verb_frames = '''
-
-clause
-    phrase function=Pred
-        word lex=JY>[ vs=hif pdp=verb
+        # helper data, for SemSpace class
+        self.target2gloss = dict()
+        self.target2lex = dict()
+        self.target2node = dict()
         
-    p1:phrase function=Objc typ=PP
-        w1:word
-        w2:word sem_domain~{good_codes}
+        # get all target/basis data
+        for param in parameters:
+            
+            for templ, templ_kw, target_i, bases_i, target_tokener, basis_tokener in param:
 
-p1 -head> w1
-w1 -prep_obj> w2
+                # run search query on template
+                search_template = templ.format(**templ_kw)
+                sample = sorted(S.search(search_template))
 
-'''.format(good_codes='|'.format(good_codes),
-          )
+                # make target token
+                for specimen in sample:
+                    clause = specimen[0]
+                    target = specimen[target_i]
+                    target_token = target_tokener(target)
+
+                    # make bases tokens, map to clause
+                    for basis_i in bases_i:
+                        basis = specimen[basis_i]
+                        basis_token = basis_tokener(basis, target)
+                        experiment_data[target_token][clause].append(basis_token)
+
+                    # add helper data
+                    self.target2gloss[target_token] = F.gloss.v(L.u(target, 'lex')[0])
+                    self.target2lex[target_token] = L.u(target, 'lex')[0]
+                    self.target2node[target_token] = target
+                
+        # finalize data
+        self.count_experiment(experiment_data)
+    
+    
+    def count_experiment(self, experiment_data):
+        '''
+        Counts experiment data into a dataframe from an
+        experiment data dictionary structured as:
+        experiment_data[target_token][clause][list_of_bases_tokens]
+        
+        --input--
+        dict
+        
+        --output--
+        pandas df
+        '''
+        
+        ecounts = collections.defaultdict(lambda: collections.Counter())
+        
+        for target, clauses in experiment_data.items():
+            for clause, bases in clauses.items():
+                ecounts[target].update(bases)
+                
+        counts = dict((target, counts) for target, counts in ecounts.items()
+                                if sum(counts.values()) >= self.min_obs)
+        
+        self.data = pd.DataFrame(counts).fillna(0)
+        self.raw_data = experiment_data
+        
+        
+class ExperimentFrame(Experiment):
+    
+    '''
+    Identical to Experiment except
+    it builds frames as basis elements.
+    '''
+    
+    def __init__(self, parameters, tf=None, min_observation=10):
+        super.__init__(parameters, tf=tf, min_observation=min_observation)
+        
+    def count_experiment(self, experiment_data):
+        '''
+        Counts experiment data into a dataframe from an
+        experiment data dictionary structured as:
+        experiment_data[target_token][clause][list_of_bases_tokens]
+        
+        *special*: Counts clauses as a single frame.
+        
+        --input--
+        dict
+        
+        --output--
+        pandas df
+        '''
+        
+        ecounts = collections.defaultdict(lambda: collections.Counter())
+        
+        for target, clauses in experiment_data.items():
+            for clause, bases in clauses.items():
+                frame = '|'.join(sorted(bases))
+                ecounts[target][frame] += 1
+                
+        counts = dict((target, counts) for target, counts in ecounts.items()
+                                if sum(counts.values()) >= self.min_obs)
+        
+        self.data = pd.DataFrame(counts).fillna(0)
+        self.raw_data = experiment_data
